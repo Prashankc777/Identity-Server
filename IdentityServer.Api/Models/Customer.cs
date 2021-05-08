04ca100422a5270ffbf6c6fc5bbf434f56f2e000
Prashan Raj Kc <Prashankc@aria.com.np>
2021-05-08 10:47:06 +0545

    Add project files.

---
+++ IdentityServer.Api/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Api.Models
+{
+    public class Customer
+    {
+
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
